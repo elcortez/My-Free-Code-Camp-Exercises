0789740d0311a55ba35607da6c01da2784df7771
Pierre Hersant <Pierre Hersant>
2016-03-25 20:10:18 +0100

    exo 12

---
+++ Basic Algorithm Scripting/012 Mutations.js
@@ -0,0 +1,9 @@
+function mutation(arr) {
+  var x = arr[0].toLowerCase().split('');
+  var y = arr[1].toLowerCase().split('');
+  
+  for (i=0;i<y.length;i++) {
+    if (x.indexOf(y[i]) < 0) {return false;}
+  }  
+  return true; 
+}
