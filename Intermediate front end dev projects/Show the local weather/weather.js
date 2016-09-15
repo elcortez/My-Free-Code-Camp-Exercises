d22d193f49b44f5c93973bba64381bb31021eef4
pierre hersant <pierre.hersant@gmail.com>
2016-09-15 16:24:43 +0200

    work in progress

--- Intermediate front end dev projects/Show the local weather/weather.js
+++ Intermediate front end dev projects/Show the local weather/weather.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
   $('#getWeather').on('click', function(){
-    var getIP = 'http://ip-api.com/json/';
-    $.getJSON(getIP).done(function(location) {
-      $('#currentLocation').text(location.city + ', ' + location.region + ', ' + location.country);
-      $.getJSON(openWeatherMap, {
-        lat: location.lat,
-        lon: location.lon,
-        units: 'metric',
-        APPID: a9c241803382387694efa243346ec4d7
-      })
-      $('#currentLocation').text(Math.round(weather.main.temp));
-    })
+    myLatAndLon(function(result) {
+      var lat = result[0];
+      var lon = result[1];
+      console.log(myWeather(lat, lon));
+      // Here, although the params work in browser, the message that gets returned in console is : "NS_ERROR_DOM_BAD_URI: Access to restricted URI denied"
+    });
   })
 })
+
+function myLatAndLon(callback) {
+  $.getJSON('http://ip-api.com/json/').done( function(location) {
+    var arr = [];
+    arr.push(location.lat);
+    arr.push(location.lon);
+    callback(arr);
+  });
+}
+
+function myWeather(lat, lon) {
+  return $.getJSON('api.openweathermap.org/data/2.5/weather', {
+      lat: lat,
+      lon: lon,
+      APPID: 'a9c241803382387694efa243346ec4d7'
+  })
+  // The params are good, and when I type them on my browser, everything works fine
+}
+
