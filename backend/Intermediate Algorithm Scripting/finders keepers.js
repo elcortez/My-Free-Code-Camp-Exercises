5be917392b548967aa4196ae784001796617d7d7
Pierre Hersant <pierre.hersant@gmail.com>
2017-01-05 18:07:06 +0100

    finders keepers

---
+++ backend/Intermediate Algorithm Scripting/finders keepers.js
@@ -0,0 +1,12 @@
+// Create a function that looks through an array (first argument) and returns
+// the first element in the array that passes a truth test (second argument).
+
+function findElement(arr, func) {
+  for(i=0; i<arr.length; i++) {
+    if(func (arr[i])) {
+      return arr[i];
+    }
+  }
+}
+
+console.log(findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; }));
