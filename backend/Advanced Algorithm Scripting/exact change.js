001425954e2ed0c47de73bcab4e53a5623cbcd0a
Pierre Hersant <pierre.hersant@gmail.com>
2017-01-27 18:45:57 +0100

    wip

---
+++ backend/Advanced Algorithm Scripting/exact change.js
@@ -0,0 +1,45 @@
+// Design a cash register drawer function checkCashRegister() that accepts purchase
+// price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+// cid is a 2D array listing available currency.
+// Return the string "Insufficient Funds" if cash-in-drawer is less than the change due.
+// Return the string "Closed" if cash-in-drawer is equal to the change due.
+// Otherwise, return change in coin and bills, sorted in highest to lowest order.
+
+function checkCashRegister(price, cash, cid) {
+  var coinValues = {
+    'PENNY': 0.01,
+    'NICKEL': 0.05,
+    'DIME': 0.1,
+    'QUARTER': 0.25,
+    'ONE': 1,
+    'FIVE': 5,
+    'TEN': 10,
+    'TWENTY': 20,
+    'ONE HUNDRED': 100
+  }
+
+  moneyToGiveBack = cash - price;
+
+  totalMoneyInCashRegister = 0;
+  cid.forEach(function(drawer){
+    totalMoneyInCashRegister += drawer[1]
+  })
+
+  if(totalMoneyInCashRegister === moneyToGiveBack) {
+    return 'Closed'
+  } else if(totalMoneyInCashRegister < moneyToGiveBack) {
+    return 'Insufficient Funds'
+  } else {
+    return coinValues
+  }
+}
+
+
+console.log(checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]])) // should return [["QUARTER", 0.50]].
+// console.log(checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]])) // should return an array.
+// console.log(checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])) // should return a string.
+// console.log(checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])) // should return a string.
+// console.log(checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]])) // should return [["TWENTY", 60.00], ["TEN", 20.00], ["FIVE", 15.00], ["ONE", 1.00], ["QUARTER", 0.50], ["DIME", 0.20], ["PENNY", 0.04]].
+// console.log(checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])) // should return "Insufficient Funds".
+// console.log(checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1.00], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])) // should return "Insufficient Funds".
+// console.log(checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])) // should return "Closed".
