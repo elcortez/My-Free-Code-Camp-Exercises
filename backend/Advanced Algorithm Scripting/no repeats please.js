d44806acb7f51c8baa950175c0c8db2a4f5fecb8
Pierre Hersant <pierre.hersant@gmail.com>
2017-02-10 18:53:50 +0100

    no repeats please

--- backend/Advanced Algorithm Scripting/no repeats please.js
+++ backend/Advanced Algorithm Scripting/no repeats please.js
@@ -7,48 +7,44 @@
 
 
 function permAlone(str) {
-  var permutations = permutateMe(str);
+  var allPermutations = permutateMe(str);
+  counter = 0
+
+  allPermutations.forEach(function(elmt){
+    if(!(elmt.match(/(.)\1+/g))) {
+      counter += 1
+    }
+  })
+  return counter;
 }
 
-// console.log(permAlone("aab")) // should return a number.
+function permutateMe(str) {
+  var arr = str.split('');
+  var allPossiblePermutations = [];
+
+  function generatePermutations(count) {
+    if(count === 1) {
+      allPossiblePermutations.push(arr.join(''));
+    } else {
+      for(var i = 0; i < count; i++){
+        generatePermutations(count - 1);
+        var swap = count % 2 ? i : 0;
+        [arr[swap], arr[count - 1]] = [arr[count - 1], arr[swap]]
+      }
+    }
+  }
+
+  generatePermutations(arr.length);
+  return allPossiblePermutations;
+}
+
+console.log(permAlone("aab")) // should return a number.
 // console.log(permAlone("aab")) // should return 2.
 // console.log(permAlone("aaa")) // should return 0.
-// console.log(permAlone("aabb")) // should return 8.
-// console.log(permAlone("abcdefa")) // should return 3600.
-// console.log(permAlone("abfdefa")) // should return 2640.
+console.log(permAlone("aabb")) // should return 8.
+console.log(permAlone("abcdefa")) // should return 3600.
+console.log(permAlone("abfdefa")) // should return 2640.
 // console.log(permAlone("zzzzzzzz")) // should return 0.
 // console.log(permAlone("a")) // should return 1.
 // console.log(permAlone("aaab")) // should return 0.
 // console.log(permAlone("aaabb")) // should return 12.
-
-var finalArr = [];
-var tempArr = [];
-
-function permute(input) {
-  console.log('------------------- starting method with i = ' + i + ', ch = ' + ch + ', chars = ' + chars + ', finalArr = ' + finalArr + ' and tempArr = ' + tempArr)
-  console.log(' ')
-  var i;
-  var ch;
-  var chars = input.split('');
-
-  for (i = 0; i < chars.length; i++) {
-    console.log('**** starting iteration with i = ' + i + ', ch = ' + ch + ', chars = ' + chars + ', finalArr = ' + finalArr + ' and tempArr = ' + tempArr)
-    ch = chars.splice(i, 1);
-
-    tempArr.push(ch);
-
-    if (chars.length == 0) {
-      finalArr[finalArr.length] = tempArr.join('');
-    }
-
-    permute(chars.join(''));
-    chars.splice(i, 0, ch);
-    tempArr.pop();
-    console.log('......................ending iteration......................................')
-    console.log(' ')
-  }
-  console.log('.......................ending method...........................')
-  return finalArr
-}
-
-console.log(permute("abc")); // should return ["abc", "acb", "bac", "bca", "cab", "cba"]
